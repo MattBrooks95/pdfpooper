ce87b12b43259929cd575b642cc026ad7bc42a37
MattBrooks95 <28607360+MattBrooks95@users.noreply.github.com>
2022-02-25 21:44:34 +0900

    source comments

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+//set up a quick web server
 const express = require('express');
+//allows for parsing multiple files out of a POST request
 const multer = require('multer');
+//allows me to manipulate a headless chrome instance, and use its
+//DOM to dynamically fill in text fields and then turn the page into a PDF
 const puppeteer = require('puppeteer');
 
+//I guess I don't need this with Express?
 //const hostname = '127.0.0.1';
+//this should be made to be configurable
 const port = 8000;
 
 const app = express();
@@ -11,9 +17,6 @@ const multiPartFormMiddleware = multer({
 		fileSize: 1e6,//1MB?
 	}
 });
-
-app.use(express.json());
-app.use(express.urlencoded());
 
 const fields = [
 	{
@@ -32,15 +35,18 @@ app.post('/makepdf', multiPartFormMiddleware.fields(fields), async (request, res
 	const templateFile = request.files.template[0];
 	const json = JSON.parse(dataFile.buffer);
 	const html = templateFile.buffer;
-	console.log('json:', json);
+	//console.log('json:', json);
 	//console.log('html:', html.toString());
-	//console.log(dataFile, templateFile);
 
 	const keyValuePairs = json;
 
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	page.setContent(html.toString());
+	//okay so the 2nd argument passed to page.evaluate is the object that will be passed
+	//into the headless chrome instance as serialized JSON
+	//so, it needs to be serializeable, and the callback that is ran on the browser side
+	//seems like it needs to have that data listed as a parameter to the function
 	await page.evaluate((keyValuePairs) => {
 		keyValuePairs.forEach((keyValuePair) => {
 			const targetElement = document.getElementById(keyValuePair.id);
@@ -65,9 +71,12 @@ app.post('/makepdf', multiPartFormMiddleware.fields(fields), async (request, res
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}. post.sh is an example on how to send over the HTML template and JSON dictionary`);
 });
 
+//before I decided on using Express, I was gonna try to do it with just Node
+//started to look unnecessarily difficult and the documentation/examples
+//that I was finding were deprecated
 //const server = http.createServer((request, response) => {
 //	response.statusCode = 200;
 //	response.setHeader('Content-Type', 'text/plain');
